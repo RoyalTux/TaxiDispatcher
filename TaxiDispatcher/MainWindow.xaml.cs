0864a754004b62988c6f23a361ad57ed6e40928f
Дмитрий <@DESKTOP-BHOC453>
2018-07-03 12:06:19 +0300

    some fix

--- TaxiDispatcher/MainWindow.xaml.cs
+++ TaxiDispatcher/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,8 +28,36 @@ namespace TaxiDispatcher
 
         private void Button_ShowOrderStatus_Click(object sender, RoutedEventArgs e)
         {
-            OrderTrackForm orderTrackForm = new OrderTrackForm();
-            orderTrackForm.Show();
+            //OrderTrackForm orderTrackForm = new OrderTrackForm();
+            //orderTrackForm.Show();
+            int result;
+            if (int.TryParse(TextBox_OrderStatus.Text, out result))
+            {
+                var orderData = DataBaseHelper.GetCurrentDataBase().TryGetOrderDataByValidTrackingNumber(result);
+                if (orderData != null)
+                {
+                    this.Close();
+                    var th = new Thread(x => {
+                        OrderTrackForm orderTrackForm = new OrderTrackForm();
+                        orderTrackForm.Show();
+                    });
+                    th.SetApartmentState(ApartmentState.STA);
+                    th.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный номер");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Недопустимые символы!");
+            }
+        }
+
+        private void TextBox_OrderStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Button_ShowOrderStatus.IsEnabled = true;
         }
     }
 }
